7770f8ee1a0b17f265125541b6a4163e6d5b8d7f
MARYDS <mary.forde.2017@gmail.com>
2020-10-10 17:48:13 +0100

    Updates

---
+++ precache-manifest.77b408393db03165329765849ad866c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4aff867a5e9665e5c0f8f04b5407ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e4c9fc5fd03c85fd858",
+    "url": "/static/css/main.34bd5715.chunk.css"
+  },
+  {
+    "revision": "9d1a57f5e2f2dafddd92",
+    "url": "/static/js/2.6c03b137.chunk.js"
+  },
+  {
+    "revision": "c83eaf460a8f55ef068d9679da1b7145",
+    "url": "/static/js/2.6c03b137.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e4c9fc5fd03c85fd858",
+    "url": "/static/js/main.146565b4.chunk.js"
+  },
+  {
+    "revision": "b7550932cdc1310782ca",
+    "url": "/static/js/runtime-main.1142ef6f.js"
+  }
+]);
