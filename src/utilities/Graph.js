d4eb8d094ae603dea1b46f3d0a6edcbd290afb7a
MARYDS <mary.forde.2017@gmail.com>
2020-10-10 17:42:34 +0100

    First Version

--- src/utilities/Graph.js
+++ src/utilities/Graph.js
@@ -8,39 +8,33 @@ export default class Graph extends Component {
 
   render() {
 
-    const data = this.props.data.map((day) => {
-      const date = (new Date(day.date))
-      return (
-        {
-          date: date.toLocaleDateString(),
-          count: day.count
-        }
-      )
-    })
-
     function CustomTooltip({ payload, label, active }) {
       if (active) {
         return (
           <div className="custom-tooltip">
-            <span className="label">{payload[0].payload.date} :</span>
-            <span className="data">{payload[0].value}</span>
+            <span className="label">{(new Date(payload[0].payload.date)).toLocaleDateString()} </span>
+            <span className="data">{(payload[0].value).toLocaleString()}</span>
           </div>
         );
       }
       return null;
     }
+    const formatXAxis = (tickItem) => {
+      const d = new Date(tickItem);
+      return d.toLocaleString('default', { month: 'short' });
+    };
 
     return (
       <div className="line-chart-wrapper">
         <LineChart
-          width={300} height={200} data={data}
+          width={300} height={200} data={this.props.data}
           margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
         >
           <CartesianGrid stroke="#ccc" vertical={false} />
-          <YAxis />
-          <Tooltip content={<CustomTooltip />}
-            labelStyle={{ fontWeight: 'bold', color: '#666666' }}
-          />
+          <YAxis tick={{ fontSize: '0.8rem' }} />
+          <XAxis interval={30} tickFormatter={formatXAxis}
+            tick={{ fontSize: '0.8rem' }} dataKey="date" />
+          <Tooltip content={<CustomTooltip />} />
           <Line type="monotone" dataKey="count" stroke="#ff7300" dot={false} />
         </LineChart>
       </div>
