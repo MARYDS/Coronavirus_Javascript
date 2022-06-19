a5a1ad81010b12350ca58cfc5a77dd59691606fa
MARYDS <mary.forde.2017@gmail.com>
2022-06-19 17:54:01 +0100

    Updates

--- precache-manifest.afbca2852eed141c781fc18709596e78.js
+++ precache-manifest.afbca2852eed141c781fc18709596e78.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7ecaec1c1e78423986808ff0f5fe276c",
+    "revision": "cb547302e420621b978f99e838b10d83",
     "url": "/Coronavirus_Javascript/index.html"
   },
   {
-    "revision": "bbf8603fa1ee1b1f0ecd",
+    "revision": "b300867427c4ec3be6b6",
     "url": "/Coronavirus_Javascript/static/css/main.c77b3f70.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Coronavirus_Javascript/static/js/2.f0193f78.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bbf8603fa1ee1b1f0ecd",
-    "url": "/Coronavirus_Javascript/static/js/main.bf687377.chunk.js"
+    "revision": "b300867427c4ec3be6b6",
+    "url": "/Coronavirus_Javascript/static/js/main.d47c8bc3.chunk.js"
   },
   {
     "revision": "347444bae6750f0be521",
